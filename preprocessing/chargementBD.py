33a50fae594463c9aefabcca87bc19cc2cd47723
PaulineVarin <pauline.varin@outlook.fr>
2022-03-02 14:17:37 +0100

    Fin nettoyage sous-titres
    Sur les fichiers de sous-titres, les éléments supprimés sont les suivants :
    
    - Phrases vides
    - Indications de placement des sous-titres (str et sub)
    - La ponctuation
    - Les mots de liaison (avec détection automatique de la langue)
    
    Notes :
    - réorganisation des fichiers pour mettre de côté mes notes et remarques
    - pour vendredi consulter les notes sur le résultat du filtrage

--- preprocessing/chargementBD.py
+++ preprocessing/chargementBD.py
@@ -1,11 +1,11 @@
+from unittest.case import DIFF_OMITTED
 import nltk
 import os
 import psycopg2
 from nltk.corpus import stopwords
 import re
 import string 
-stopwords.words('english') #a modifier dynamiquement en fonction de la langue du fichier
-
+import langid # pip install langid==1.1.6
 
 
 #Début déclaration méthodes
@@ -53,16 +53,60 @@ def tester_pattern(pattern, string_test):
 def traitement_ponctuation(fichier_phrases) :
     phrases_temp = []
     for phrase in fichier_phrases:
-        phrases_temp.append(re.sub("[^\w\s']", ' ',phrase))
+        #Suppression de la ponctuation
+        tem_p = re.sub("[^\w\s']", ' ',phrase)
+        #Suppression des espaces en trop
+        phrases_temp.append(re.sub('\s+', ' ', tem_p))
     return phrases_temp
 
 
-# IN  : 
-# OUT :
+# IN  : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série
+# OUT : La langue du fichier de sous-titres
+def definition_langue_fichier(fichier_phrases):
+
+    #Création d'un dictionnaire pour stocker les résultats de la détection de la langue 
+    dict_lg = {'en':0,'fr':0}
+
+    #On récupère seulement une partie des phrases pour tester
+    nb_phrases = int(len(fichier_phrases)/3)
+    phrases_test = fichier_phrases[0:nb_phrases]
+
+    #Identification de la langue
+    for phrase in phrases_test :
+        lg = langid.classify(phrase)[0]
+        if lg == 'en':
+            dict_lg["en"] = dict_lg["en"] + 1
+        elif lg =='fr':
+            dict_lg["fr"] = dict_lg["fr"] + 1
+    
+    if dict_lg["en"]>dict_lg["fr"] : 
+        return "english"
+    else: 
+        return "french"
+
+
+# IN  : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série avec des phrases sans indications de sous-titres et de ponctuation
+# OUT : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série sans les mots de liaison
 def traitement_stop_words(fichier_phrases) :
-    return fichier_phrases
 
+    #Détection de la langue du fichier de sous-titres que l'on traite
+    langue  = definition_langue_fichier(fichier_phrases)
+    liste_stop_w = stopwords.words(langue)
+   
+    fichier_phrases_temp = []
+    #Suppression des mots de liaison
+    for phrase in fichier_phrases :
+        print(phrase)
+        temp_phrase = [mot for mot in phrase.lower().split() if mot not in liste_stop_w]
+        print(temp_phrase)      
+        #Re-création de la phrase en une chaine
+        temp_phrase = ' '.join(temp_phrase)
+        #Ajout au tableau
+        fichier_phrases_temp.append(temp_phrase)
 
+    print(fichier_phrases_temp)
+    print("------------------")
+    return fichier_phrases_temp
 
 
 #IN  : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série, le nom du fichier en cours de traitement avec l'extension
@@ -87,9 +131,9 @@ def nettoyage_fichier_st (fichier_phrases,nom_fichier):
     elif os.path.splitext(nom_fichier)[1]==".sub" :
         for phrase in fichier_phrases : 
             fichier_phrases_temp.append(re.sub('^{[0-9]+}{[0-9]+}', '', phrase))
-
+            
     fichier_phrases_temp = traitement_ponctuation(fichier_phrases_temp)
-    #fichier_phrases_temp = traitement_stop_words(fichier_phrases_temp)
+    fichier_phrases_temp = traitement_stop_words(fichier_phrases_temp)
 
     fichier_propre = fichier_phrases_temp
 
@@ -156,7 +200,7 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 #Récupération du nom de toutes les entrées contenues dans le dossier sous-titres (ici on va tester avec un dossier plus petit)
-chemin_dossier = 'D:\\LP\\ProjetLP\\sous-titres'
+chemin_dossier = 'D:\\LP\\ProjetLP\\sr_test'
 noms_series = os.listdir(chemin_dossier)
 nb_series = 0
 
@@ -176,26 +220,24 @@ for serie in noms_series :
             #fermer le fichier
             fileObj.close()
 
-            #Nettoyer les phrases récupérées du fichier de sous-titre en fonction du type de fichier (srt ou sub) et ajout du fichier au dataset
-            fichier_ok = nettoyage_fichier_st(fichier_phrases,fichier.name)
-            dataset.append(fichier_ok)
-
-
-            #Affichage pour vérifier le résultat de chaque fichier (1 fichier = 1 array)
-            #print(fichier.name)
-            print("---------------------")
-            print(fichier_ok)
-            print("---------------------")
-
-    print(len(dataset))
-    print("Fin serie : "+serie)
+            #Nettoyer les phrases récupérées du fichier de sous-titre (si il n'est pas vide) en fonction du type de fichier (srt ou sub) et ajout du fichier au dataset
+            if len(fichier_phrases) !=0 :
+                fichier_ok = nettoyage_fichier_st(fichier_phrases,fichier.name)
+                dataset.append(fichier_ok)
+                #Affichage pour vérifier le résultat de chaque fichier (1 fichier = 1 array)
+                #print("---------------------")
+                #print(fichier_ok)
+                #print("---------------------")
+            
+    #print(len(dataset))
+    #print("Fin serie : "+serie)
     #print(dataset)
-    print("---------------------")
+    
     #Calcul du TF-IDF sur le dataset 
         #Calcul de l'occurence des mots qui selon le TD-IDF sont pertinents pour décrire la série
         #Chargement dans la BDD
             #Table Serie
             #Table Mot
             #Table Contenir
-print("fin traitement")
-print(nb_series)
+#print("fin traitement")
+#print(nb_series)
