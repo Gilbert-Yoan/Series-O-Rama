b87f6a32059cd74f430d7929ca42d6e4e6d10b82
PaulineVarin <pauline.varin@outlook.fr>
2022-05-01 12:41:24 +0200

    Mise en place TFIDF - MAJ
    Remise en place du TFIDF avec la solution présente dans le fichier test tfidf.py
    
    Remise en place des fonctions de traitement des fichiers de sous-titres
    - Suppression des indications de sous-titres : OK
    - Suppression de la ponctuation : OK
    - Passage des phrases en minuscule : OK
    - Traitement Stop Words : OK
    - Lemmatisation : OK
    - Tri par valeur : OK

--- preprocessing/tfidf_test_pauline.py
+++ preprocessing/tfidf_test_pauline.py
@@ -1,8 +1,12 @@
 #Liste imports
 import os
 from math import *
-from nltk.corpus import stopwords
 import re 
+import nltk
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+
 
 #1 DOCUMENT = 1 SERIE 
 #Définiton fonctions nettoyage fichiers
@@ -15,7 +19,7 @@ def tester_pattern(pattern, string_test):
         return False #match
 
 #IN  : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série, le nom du fichier en cours de traitement avec l'extension
-#OUT : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série et la langue du fichier
+#OUT : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série
 def nettoyage_fichier_st (fichier_phrases,nom_fichier):
     fichier_phrases_temp=[]
 
@@ -30,6 +34,14 @@ def nettoyage_fichier_st (fichier_phrases,nom_fichier):
         fichier_phrases_temp=[phrase for phrase in fichier_phrases if tester_pattern(pattern, phrase)]
         fichier_phrases_temp=[phrase for phrase in fichier_phrases_temp if tester_pattern(pattern2, phrase)]
 
+        #Supprimer les indications de formatage du texte
+        pattern3 = "^<font color=.*$"
+        pattern4 = "^.*<\/font>$"
+
+        fichier_phrases_temp=[phrase for phrase in fichier_phrases_temp if tester_pattern(pattern3, phrase)]
+        fichier_phrases_temp=[phrase for phrase in fichier_phrases_temp if tester_pattern(pattern4, phrase)]
+
+
     elif os.path.splitext(nom_fichier)[1]==".sub" :
         for phrase in fichier_phrases : 
             fichier_phrases_temp.append(re.sub('^{[0-9]+}{[0-9]+}', '', phrase))
@@ -37,28 +49,64 @@ def nettoyage_fichier_st (fichier_phrases,nom_fichier):
     return fichier_phrases_temp 
 
 
+# IN  : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série
+# OUT : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série sans la ponctuation
+#https://www.geeksforgeeks.org/python-lemmatization-approaches-with-examples/
+#https://www.holisticseo.digital/python-seo/nltk/lemmatize
+
+def pos_tagger(nltk_tag):
+    if nltk_tag.startswith('J'):
+        return wordnet.ADJ
+    elif nltk_tag.startswith('V'):
+        return wordnet.VERB
+    elif nltk_tag.startswith('N'):
+        return wordnet.NOUN
+    elif nltk_tag.startswith('R'):
+        return wordnet.ADV
+    else:         
+        return None
 
 # IN  : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série
 # OUT : Un array contenant l'ensemble des phrases d'un fichier de sous-titres pour une série sans la ponctuation  
 def traitement_mots(fichier_phrases) :
     phrases_temp = []
+
     #Traitement de la ponctuation
     for phrase in fichier_phrases:
         #Suppression de la ponctuation
         tem_p = re.sub("[^\w\s']", ' ',phrase)
         tem_p = re.sub("[.*']",' ',tem_p)
-        #Suppression des espaces en trop (a regarder)
         phrases_temp.append(re.sub('\s+', ' ', tem_p))
 
     #Passage des mots en minuscule
     for indice in range(len(phrases_temp)):
         phrases_temp[indice] = phrases_temp[indice].lower()
 
-    #Suppression des stop words
+    
+    #Lemmatisation des mots
+    lemmatizer = WordNetLemmatizer()
+    for indice_phrase in range(len(phrases_temp)):
+        #print(phrases_temp[indice_phrase])
+        # découpage de la chaine en mots et définition du POS Tag pour chaque mot
+        pos_tagged = nltk.pos_tag(nltk.word_tokenize(phrases_temp[indice_phrase])) 
+        
+        #Transformation des POS Tag de chaque mot en WordNet tag pour la suite
+        wordnet_tagged = list(map(lambda x: (x[0], pos_tagger(x[1])), pos_tagged))
 
-    #Lemmatisation des mots 
+        #Lemmatisation de chaque mot, reconstitution en phrase et rajout à la liste
+        lemmatized_sentence = []
+        for word, tag in wordnet_tagged:
+            if tag is None:
+                # cas si le tag n'existe pas
+                lemmatized_sentence.append(word)
+            else:       
+                # cas si le tag existe
+                lemmatized_sentence.append(lemmatizer.lemmatize(word, tag))
 
-    #
+        lemmatized_sentence = " ".join(lemmatized_sentence)
+
+        phrases_temp[indice_phrase] = lemmatized_sentence
+
     return phrases_temp
 
 
@@ -67,7 +115,9 @@ def traitement_mots(fichier_phrases) :
 #Définition fonctions TFIDF
 
 def calculer_tf(documents):
+    #Cntient le nombre d'apparitons de chaque terme dans le corpus entier
     tf = []
+    #Contient le nombre de series ou le mot apparait 
     df = {}
     sw = stopwords.words('english')
     cur_index = 0
@@ -90,8 +140,7 @@ def calculer_tf(documents):
 def calculer_idf(documents,df):
     idf = {}
     for mot in df:
-        #Ajout de 1 pour éviter le 0
-        idf[mot] = 1 + log(len(documents) / df[mot],2)
+        idf[mot] = log(len(documents) / df[mot],2)
     return idf
 
 def calculer_tf_idf(tf,idf):
@@ -137,13 +186,15 @@ for serie in noms_series :
     #print('LISTE TEMP')
     #print(liste_temp)
     print("Fin serie : "+serie)
-    #Transformation de la serie en une chaine de caractère
+    #Transformation de la serie en une chaine de caractère et ajout à la liste qui contient toutes les series 
     liste_series.append(' '.join(liste_temp))
 
-print(liste_series)
+#print(liste_series)
 i = 0
 while i < len(liste_series): 
-    liste_series[i] = liste_series[i].split(" ")
+    #Suppression des espaces en trop
+    liste_series[i] = re.sub('\s+', ' ', liste_series[i])
+    liste_series[i] = liste_series[i].split(' ')
     i += 1
 
 #print(liste_series)
@@ -158,5 +209,13 @@ print(calculer_tf_idf(calculer_tf(liste_series)[0],calculer_idf(liste_series,cal
 
 res_tfidf = calculer_tf_idf(calculer_tf(liste_series)[0],calculer_idf(liste_series,calculer_tf(liste_series)[1]))
 
-#for element in res_tfidf:
-    #print (sorted(element,reverse = True))
+for serie in res_tfidf:
+    for key, value in sorted(serie.items(), reverse=True, key=lambda item: item[1]):
+        print(key,value)
+
+    print('Serie suivante')
+
+
+#A REGARDER
+# - Identification de la serie à la fin du TFIDF
+#
