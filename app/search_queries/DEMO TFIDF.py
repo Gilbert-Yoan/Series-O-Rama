b81c653120c380189e0d898a71303bf96c18bf0f
Gilbert-Yoan <97592080+Gilbert-Yoan@users.noreply.github.com>
2022-05-30 16:46:54 +0200

    Traduction recherche query
    Modification de la requete pour la traduction Français vers Anglais

--- app/search_queries/DEMO TFIDF.py
+++ app/search_queries/DEMO TFIDF.py
@@ -1,13 +1,10 @@
 #Liste imports
 import os
-from math import *
-import re
-import nltk
-from nltk.corpus import stopwords
-from nltk.corpus import wordnet
-from nltk.stem import WordNetLemmatizer
 import psycopg2 as pg
+import json
 import sys
+import unicodedata
+import string
 
 #Liste des fichiers à importer
 curr_dir = os.path.dirname(os.path.abspath(__file__))
@@ -17,18 +14,36 @@ sys.path.append(python_target)
 from methodes_preprocessing import traitement_mots, connexionBDD, fermetureBDD
 
 
+def remove_accents(data):
+    return ''.join(x for x in unicodedata.normalize('NFKD', data) if x in string.printable).lower()
+
+
 #Ici on suppose que les search_words sont des chaines de caractères simples séparées par des espaces
 def get_search_results(cursor, search_words):
     words_list = search_words.split(' ')
-    print(words_list)
-    res = traitement_mots(words_list)
-    print(res)
+
+    #Récupération du dictionnaire FR-EN JSON
+    with open(os.path.join(curr_dir,'fr-en-dict.json'),'r',encoding='utf8') as json_file:
+        dictionnaire = json.load(json_file)
+
+    #Traduction approximative des mots tapés en Français
+    words_list_en = []
+    for word in words_list:
+        try:
+            for w in dictionnaire[remove_accents(word)] :
+                words_list_en.append(w)
+        except KeyError:
+            words_list_en.append(word)
+
+    #print(word_list_en) pour afficher les traductions réalisés par le dictionnaire
+    res = traitement_mots(words_list_en)
+    #print(res) pour afficher les résultats lemmatizés
     res = []
     for word in words_list:
         query = "WITH allDoc AS (SELECT COUNT(s.ids) total FROM serie s) , \
-                word_DF AS (SELECT COUNT(s.ids) as df FROM serie s, contenir c, mot m WHERE m.idm = c.idm AND s.ids = c.ids AND m.mot = %s), \
+                word_DF AS (SELECT COUNT(s.ids) as df FROM serie s, contenir c, mot m WHERE m.idm = c.idm AND s.ids = c.ids AND LOWER(m.mot) = LOWER(%s)), \
                 word_doc AS (SELECT c.occurence curr_occ, m.mot mot, s.ids serie_id, s.noms nom FROM serie s, contenir c, mot m \
-                        WHERE m.idm = c.idm AND s.ids = c.ids AND m.mot = %s) \
+                        WHERE m.idm = c.idm AND s.ids = c.ids AND m.mot = LOWER(%s)) \
                 SELECT serie_id, nom, mot,curr_occ tf,df, LOG(2.0, total/df) idf, curr_occ*LOG(2.0, total/df) as tfidf FROM word_df, allDoc, word_doc;"
 
         cursor.execute(query, (word, word))
@@ -42,7 +57,8 @@ def get_search_results(cursor, search_words):
 #Connexion a la BDD
 connexion, curseur = connexionBDD()
 
-res = get_search_results(curseur, "students studying studies was cavalry chivalry")
+#Test français anglais pour traduction
+res = get_search_results(curseur, "château magie studies was cavalry chivalry")
     
 print(res)
 #Fermeture de l'accès à la BDD
--- app/search_queries/csv_to_json.py
+++ app/search_queries/csv_to_json.py
@@ -1,6 +1,11 @@
 import os
+import unicodedata
+import string
 import json
 import csv
+
+def remove_accents(data):
+    return ''.join(x for x in unicodedata.normalize('NFKD', data) if x in string.printable).lower()
 
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 csv_target = os.path.join(curr_dir, "fr-en-dict.csv")
@@ -11,10 +16,10 @@ with open(csv_target,'r', encoding='utf8') as csv_file:
     print(content)
     fr_en_dict = {}
     for row in content:
-        if row[0] not in fr_en_dict:
-            fr_en_dict[row[0]] = [row[3]]
+        if remove_accents(row[0]) not in fr_en_dict:
+            fr_en_dict[remove_accents(row[0])] = [row[3]]
         else:
-            fr_en_dict[row[0]].append(row[3])
+            fr_en_dict[remove_accents(row[0])].append(row[3])
     with open(os.path.join(curr_dir,"fr-en-dict.json"),"w", encoding='utf8') as jsonfile:
         fr_en_json = json.dump(fr_en_dict, jsonfile ,ensure_ascii=False)
     
