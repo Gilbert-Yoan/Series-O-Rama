ca0b9a169050b31a666c8ca27d7000739e63ae8e
Gilbert-Yoan <97592080+Gilbert-Yoan@users.noreply.github.com>
2022-06-02 17:25:55 +0200

    Amélioration scheduler + str tfidf

--- app/BDD/scheduler_update.py
+++ app/BDD/scheduler_update.py
@@ -1,15 +1,19 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 import os
+import shutil
 
 scheduler = BlockingScheduler()
 
 def refresh_data():
     curr_dir = os.path.dirname(os.path.abspath(__file__))
     python_target = os.path.join(curr_dir,"..","preprocessing", "preprocessing_chargementBDD.py")
-    source_path = os.path.join(curr_dir,"..","delta")
+    source_path = os.path.join(curr_dir,"..","..","DELTA")
     #print("python ",python_target, source_path)
+    #Execution fouille
     subprocess.run(["python",python_target,source_path],shell=True)
+    #Suppression delta après execution
+    shutil.rmtree(source_path)
 
-scheduler.add_job(refresh_data, 'interval', seconds=5)
+scheduler.add_job(refresh_data, 'cron', hour=0, minute=1)
 scheduler.start()
--- app/search_queries/DEMO TFIDF.py
+++ app/search_queries/DEMO TFIDF.py
@@ -12,7 +12,7 @@ python_target = os.path.join(curr_dir,"..","preprocessing")
 sys.path.append(python_target)
 
 from methodes_preprocessing import traitement_mots, connexionBDD, fermetureBDD
-
+ 
 
 def remove_accents(data):
     return ''.join(x for x in unicodedata.normalize('NFKD', data) if x in string.printable).lower()
@@ -39,16 +39,28 @@ def get_search_results(cursor, search_words):
     res = traitement_mots(words_list_en)
     #print(res) pour afficher les résultats lemmatizés
     res = []
-    for word in words_list_en:
+    for word in words_list:
+        print(word)
         query = "WITH allDoc AS (SELECT COUNT(s.ids) total FROM serie s) , \
                 word_DF AS (SELECT COUNT(s.ids) as df FROM serie s, contenir c, mot m WHERE m.idm = c.idm AND s.ids = c.ids AND LOWER(m.mot) = LOWER(%s)), \
                 word_doc AS (SELECT c.occurence curr_occ, m.mot mot, s.ids serie_id, s.noms nom FROM serie s, contenir c, mot m \
-                        WHERE m.idm = c.idm AND s.ids = c.ids AND m.mot = LOWER(%s)) \
+                        WHERE m.idm = c.idm AND s.ids = c.ids AND LOWER(m.mot) = LOWER(%s)) \
                 SELECT serie_id, nom, curr_occ*LOG(2.0, total/df) as tfidf FROM word_df, allDoc, word_doc ORDER BY tfidf DESC;"
 
         cursor.execute(query, (word, word))
         res.append(cursor.fetchall())
-    return res
+
+    print(res)
+
+    res_str = ""
+    for r in res:
+        for tup in r:
+            query = "SELECT AVG(note) FROM noter WHERE ids=%s;"
+            cursor.execute(query, (tup[0],))
+            note = cursor.fetchall()
+            res_str = res_str +str(tup[0]) + "@" + tup[1] + "@" + str(note[0][0]) +";" 
+
+    return res_str[:-1]
 
 
 #--------------------------------------------------------------------------------
